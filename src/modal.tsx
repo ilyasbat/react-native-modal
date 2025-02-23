f289dc2e45e19116d8b871175630b584c5e3606a
İlyas BAT <tabsayli@gmail.com>
2025-02-23 18:58:23 +0300

    Update modal.tsx

--- src/modal.tsx
+++ src/modal.tsx
@@ -720,6 +720,7 @@ export class ReactNativeModal extends React.Component<ModalProps, State> {
           this.state.showContent && !hasCustomBackdrop
             ? backdropColor
             : 'transparent',
+         opacity: this.props.backdropOpacity
       },
     ];
 
